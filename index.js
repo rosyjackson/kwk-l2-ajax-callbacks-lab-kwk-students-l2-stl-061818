8ef7cf29f45166a287b0b9e802a79f5c823f3953
rosyjackson <rosyjackson2004@gmail.com>
2018-06-26 15:04:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-$(document).ready(function (){
+//$(document).ready(function (){
 });
+$.ajax({
+  url:'jquery-3.1.0.min.js'
+  dataType: '',
+  data: {
+    api_key: ''
+  }
+  success: resp => {
+       url = resp.data["0"].embed_url
+       $('#lonelyDiv').html(`<iframe src=${url}>`)
+})
+function searchRepositories(searchTerm) {
+  
+}
