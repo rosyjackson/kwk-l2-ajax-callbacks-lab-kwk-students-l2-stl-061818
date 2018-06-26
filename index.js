47a7c3cbf728e84fa223cd49af36c01d4776aba5
rosyjackson <rosyjackson2004@gmail.com>
2018-06-26 15:12:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //$(document).ready(function (){
 });
 $.ajax({
-  url:''
+  url:'https://developer.github.com/v3'
   dataType: 'JSON',
   data: {
     api_key: ' 8fd2e497e4442e9b4e59e72eb2c1dc7fd2e9dc49'
