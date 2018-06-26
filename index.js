d7c84b92db84daadb287b2b9f6b64f1d111d7984
rosyjackson <rosyjackson2004@gmail.com>
2018-06-26 15:24:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 $(document).ready(function (){
 });
+
+function searchRepositories(){
+ const searchTerms = $( "#searchTerms").val();
+ $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+   $(`#results`).html(data);
+ })
+
+}
+
+var renderSearchResults = (data) => data.items.map(result => renderSearchResults(result))
+
+var renderSearchResults = (result) => {
+ return `
+   <div>
+   
+   </div>'
+}
+  
