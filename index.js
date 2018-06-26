abeb92c8ec8558d68985a0a7a5e69fc1b7946632
rosyjackson <rosyjackson2004@gmail.com>
2018-06-26 15:10:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $.ajax({
   url:''
   dataType: 'JSON',
   data: {
-    api_key: ''
+    api_key: ' 8fd2e497e4442e9b4e59e72eb2c1dc7fd2e9dc49'
   }
   success: resp => {
        url = resp.data[""].embed_url
