c33448ad3dc2870a1cc002e77b4f1427a8bbe9c3
rosyjackson <rosyjackson2004@gmail.com>
2018-06-26 15:12:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //$(document).ready(function (){
-});
+//});
 $.ajax({
   url:'https://developer.github.com/v3'
   dataType: 'JSON',
@@ -7,7 +7,7 @@ $.ajax({
     api_key: ' 8fd2e497e4442e9b4e59e72eb2c1dc7fd2e9dc49'
   }
   success: resp => {
-       url = resp.data[""].embed_url
+       url = resp.data["0"].embed_url
        $('#details').html(`<iframe src=${url}>`)
 })
 //function searchRepositories(searchTerm) 
